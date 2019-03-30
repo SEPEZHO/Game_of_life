7615af3766ecbf033da597c27328d8114514294a
SEPEZHO <47150504+SEPEZHO@users.noreply.github.com>
2019-03-30 11:40:52 +0300

    asdf

--- Js.js
+++ Js.js
@@ -4,6 +4,8 @@ var mas = [];
 var count = 0;
 var timer;
 var range = document.getElementById('range');
+var width = 800;
+var height = 600;
 // При клике на игровом поле мы должны подставить героя.
 canvas.onclick = function (event) {
   var x = event.offsetX; // кордината определяется относительно кенваса
@@ -19,22 +21,38 @@ canvas.onclick = function (event) {
 }
 // Создадим игровое поле.
 function goLife() {
-  var n = 30,
-    m = 30; // Создадим массив, который имитирует игровое поле.
-  for (var i = 0; i < m; i++) {
+  var n = width,
+    m = height; // Создадим массив, который имитирует игровое поле.
+  for (var i = 0; i < n; i++) {
     mas[i] = []; // объявим пустой массив
-    for (var j = 0; j < n; j++) {
+    for (var j = 0; j < m; j++) {
       mas[i][j] = 0;
     }
   }
 }
 goLife();
 
+function stroke() {
+  for (var x = 0.5; x <= width; x += 10) {
+    ctx.moveTo(x, 0);
+    ctx.lineTo(x, width);
+  }
+  for (var y = 0.5; y <= width; y += 10) {
+    ctx.moveTo(0, y);
+    ctx.lineTo(width, y);
+  }
+  ctx.strokeStyle = 'black';
+  ctx.stroke();
+}
+stroke();
+
 function drawField() {
-  ctx.clearRect(0, 0, 300, 300);
-  for (var i = 0; i < 30; i++) {
-    for (var j = 0; j < 30; j++) {
+  ctx.clearRect(0, 0, width, height);
+  stroke();
+  for (var i = 0; i < width; i++) {
+    for (var j = 0; j < height; j++) {
       if (mas[i][j] == 1) {
+        ctx.fillStyle = 'black';
         ctx.fillRect(j * 10, i * 10, 10, 10);
       }
     }
@@ -43,9 +61,9 @@ function drawField() {
 // Моделирование жизни.
 function startLife() {
   var mas2 = [];
-  for (var i = 0; i < 30; i++) {
+  for (var i = 0; i < width; i++) {
     mas2[i] = [];
-    for (var j = 0; j < 30; j++) {
+    for (var j = 0; j < height; j++) {
       var neighbors = 0;
       if (mas[fpm(i) - 1][j] == 1) neighbors++; //up
       if (mas[i][fpp(j) + 1] == 1) neighbors++; //right
@@ -81,12 +99,12 @@ function reset() {
 }
 
 function fpm(i) {
-  if (i == 0) return 30;
+  if (i == 0) return width;
   else return i;
 }
 
 function fpp(i) {
-  if (i == 29) return -1;
+  if (i == width - 1) return -1;
   else return i;
 }
 document.getElementById('start').onclick = startLife;
